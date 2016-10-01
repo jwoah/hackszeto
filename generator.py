ef98fea813bbb4d64829f5651f600fcb3f4a8d5f
Leo Kim <lkim@localhost.localdomain>
2016-10-01 03:22:12 -0400

    Commit for Stanley

--- generator.py
+++ generator.py
@@ -2,6 +2,10 @@ from urllib2 import Request, urlopen
 import json
 
 def get_recipe():
-    request = Request('http://food2fork.com/api/search?key=4dcf9327553c0ed33a73821107e4cb73')
+    request = Request('http://food2fork.com/api/search?key=4dcf9327553c0ed33a73821107e4cb73&q=chicken,broccoli')
     response_body = urlopen(request).read()
     return json.loads(response_body)
+
+print get_recipe()
+
++
